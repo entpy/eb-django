be8e889e1af99eeb8dbbc112f44b6406377673ab
developer <developer@entpy.com>
2014-06-03 18:30:05 +0200

    working on view

--- essere_benessere/essere_benessere/urls.py
+++ essere_benessere/essere_benessere/urls.py
@@ -4,9 +4,15 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'essere_benessere.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'website.views.home', name='home'),
+    url(r'^chi-siamo/', 'website.views.chi_siamo', name='chi_siamo'),
+    url(r'^i-nostri-lavori/', 'website.views.i_nostri_lavori', name='i_nostri_lavori'),
+    url(r'^contatti/', 'website.views.contatti', name='contatti'),
+    url(r'^le-nostre-offerte/', 'website.views.le_nostre_offerte', name='le_nostre_offerte'),
+    url(r'^ricevi-offerte/', 'website.views.ricevi_offerte', name='ricevi_offerte'),
+    url(r'^luce-pulsata/', 'website.views.luce_pulsata', name='luce_pulsata'),
+    url(r'^sbiancamento-dentale/', 'website.views.sbiancamento_dentale', name='sbiancamento_dentale'),
 
+    # admin URL
     url(r'^admin/', include(admin.site.urls)),
 )
