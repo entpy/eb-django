19d816ac481e0c1dcff0d718f3c4b86d6d239919
developer <developer@entpy.com>
2014-06-04 18:32:24 +0200

    working on models

--- essere_benessere/website/models.py
+++ essere_benessere/website/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    id_account = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    mobile_phone = models.CharField(max_length=20)
+    birthday_date = models.DateField()
+    receive_promotions = models.BooleanField(default=0)
+    loyal_customer = models.BooleanField(default=0)
+    status = models.BooleanField(default=1)
+
+    # On Python 3: def __str__(self):
+    def __unicode__(self):
+        return self.id_account
+
+class Promotion(models.Model):
+    id_promotion = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    expiring_date = models.DateTimeField()
+    type = models.CharField(max_length=10)
+    status = models.BooleanField(default=1)
+    campaigns = models.ManyToManyField(Account, through='Campaign')
+
+    # On Python 3: def __str__(self):
+    def __unicode__(self):
+        return self.id_promotion
+
+class Campaign(models.Model):
+    id_campaign = models.AutoField(primary_key=True)
+    id_account = models.ForeignKey(Account)
+    id_promotion = models.ForeignKey(Promotion)
+    status = models.BooleanField(default=0)
+
+    # On Python 3: def __str__(self):
+    def __unicode__(self):
+        return self.id_campaign
