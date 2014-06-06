ffdb4aa79841a8f2a285299c50caaab2dd3e472d
developer <developer@entpy.com>
2014-06-07 01:59:47 +0200

    working on templates

--- essere_benessere/essere_benessere/settings.py
+++ essere_benessere/essere_benessere/settings.py
@@ -56,6 +56,20 @@ WSGI_APPLICATION = 'essere_benessere.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# postgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eb_website',
+        'USER': 'testuser',
+        'PASSWORD': 'testuser',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+# MySQL
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -66,6 +80,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
