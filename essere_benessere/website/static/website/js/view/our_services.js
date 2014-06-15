b9f4e27cfe79d3a986c648d7b610c941637e9c32
developer <developer@entpy.com>
2014-06-16 00:38:42 +0200

    working on template

---
+++ essere_benessere/website/static/website/js/view/our_services.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	// loading default block serivces
+	load_default_services_block();
+
+	// fancybox init
+	$(".fancybox").fancybox();
+
+	$(".serviceClickAction").on("click",  function(){
+
+		var blockNumberResult = false;
+		var blockNumber = false;
+
+		$(".serviceNameAction").removeClass("service_active");
+		$(this).addClass("service_active");
+
+		// retrieving current block number
+		blockNumberResult = $(this).attr("class").match(/@@([^@]+)@@/);
+		blockNumber = blockNumberResult[1];
+
+		load_services_block(blockNumber);
+
+		return true;
+	});
+
+	function load_default_services_block() {
+		load_services_block(1);
+
+		return true;
+	}
+
+	function load_services_block(blockNumber) {
+		if (blockNumber) {
+			$(".service_html_container").html($(".html_block_service_" + blockNumber).html());
+		}
+
+		return true;
+	}
+});
