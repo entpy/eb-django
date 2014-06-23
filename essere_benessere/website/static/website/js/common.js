e035db7f50bc9ab6b6b36afa9e2c6067b6ffaa17
developer <developer@entpy.com>
2014-06-23 22:47:17 +0200

    css fix

--- essere_benessere/website/static/website/js/common.js
+++ essere_benessere/website/static/website/js/common.js
@@ -1,7 +1,16 @@
+// global vars section
+var isMobileView = false;
+
 $(document).ready(function(){
 
 	// showing/hiding top menu
 	$(document).on( "click", ".toggleMenuAction", function() {
 		$(".nav_container").slideToggle();
 	});
+
+	if ($(window).width() <= 745) {
+		isMobileView = true;
+	}
+
+	// TODO: if offset.top > 40px, then showing arrow up
 });
--- essere_benessere/website/static/website/js/view/our_services.js
+++ essere_benessere/website/static/website/js/view/our_services.js
@@ -13,12 +13,16 @@ $(document).ready(function(){
 
 		$(".serviceNameAction").removeClass("service_active");
 		$(this).addClass("service_active");
-
 		// retrieving current block number
 		blockNumberResult = $(this).attr("class").match(/@@([^@]+)@@/);
 		blockNumber = blockNumberResult[1];
 
 		load_services_block(blockNumber);
+
+		// vertical scolling to this element
+		if (isMobileView) {
+			$("html").scrollTop($(this).offset().top);
+		}
 
 		return true;
 	});
